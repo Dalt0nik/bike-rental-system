11954b295d0e8768bc82f1bb8035c1194550cfba
Evgenij Shapovalov <zhenya.shapovalov@gmail.com>
2025-05-03 00:35:35 +0300

    SCRUM-20: Use automapper in BikeService

--- bike-rental-be/src/main/java/lt/psk/bikerental/DTO/Bike/BikeDTO.java
+++ bike-rental-be/src/main/java/lt/psk/bikerental/DTO/Bike/BikeDTO.java
@@ -1,11 +1,15 @@
 package lt.psk.bikerental.DTO.Bike;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lt.psk.bikerental.entity.BikeState;
 
 import java.util.UUID;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Builder
 public class BikeDTO {
