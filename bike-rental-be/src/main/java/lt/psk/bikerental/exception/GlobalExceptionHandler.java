baedfcd993f48d5777751d9f9e382830a8fa79a3
ashkema <a_hovhannisyan@wargaming.net>
2025-05-03 00:04:11 +0300

    Bike crud (#5)
    * SCRUM-15: add bike crud
    
    * SCRUM-15: add bike crud postman tests
    
    * SCRUM-15: add exception handler
    
    * SCRUM-15: add getAllBikesByStationId endpoint
    
    * SCRUM-15: remove short-unique-name
    
    * SCRUM-15: add bulk bike create

---
+++ bike-rental-be/src/main/java/lt/psk/bikerental/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package lt.psk.bikerental.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ex.getMessage());
+    }
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
+}
