5f07fb8812cd6543560b16f6a30b02cfd47e8a7b
Artiom Hovhannisyan <a_hovhannisyan@wargaming.net>
2025-06-02 22:32:10 +0300

    add check calculation when trip is finished

--- bike-rental-be/src/main/java/lt/psk/bikerental/service/TripService.java
+++ bike-rental-be/src/main/java/lt/psk/bikerental/service/TripService.java
@@ -34,6 +34,7 @@ public class TripService {
     private final BookingService bookingService;
     private final ModelMapper mapper;
     private final TripValidator tripValidator;
+    private final BillingService billingService;
 
     @Transactional
     public TripDTO startTrip(CreateTripDTO dto, Jwt jwt) {
@@ -87,6 +88,8 @@ public class TripService {
 
         bikeRepository.save(trip.getBike());
         tripRepository.save(trip);
+
+        billingService.createAndSaveCheck(user, trip.getBooking(), trip);
         // TODO: websocket event
     }
 }
